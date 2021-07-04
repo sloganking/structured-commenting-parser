bf31f670c7881eae13301eb60a328ab4c61dde5a
Logan King <seanloganking@gmail.com>
2021-07-04 19:15:37 -0400

    submit to clippy

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn count_tabs(line: &str) -> usize {
             return (c_count) / 4;
         }
     }
-    return c_count / 4;
+    c_count / 4
 }
 
 fn line_is_a_comment(line: &str) -> bool {
@@ -54,72 +54,30 @@ fn main() {
         // remove all '/r'
             let lines: Vec<String> = lines.into_iter().map(|x| x.replace("\r", "")).collect();
 
-        // for line in lines.iter() {
-        //     println!("\n{}", line);
-        //     println!("{}", count_tabs(line));
-        //     println!("line_is_a_comment: {}", line_is_a_comment(line));
-        // }
-
-        // println!("{:?}", lines);
-
     // assemble tree
         let mut comment_closing_depths: Vec<usize> = Vec::new();
-
         let mut tree = TreeBuilder::new("code".to_string());
-
-        let mut begin_child_count = 0;
-        let mut end_child_count = 0;
 
         for line in lines {
             let current_depth = count_tabs(&line);
 
             // see if comments need closing
-                if comment_closing_depths.len() > 0 && contains_non_space(&line){
-                    while comment_closing_depths.len() > 0 && current_depth <= comment_closing_depths[comment_closing_depths.len() - 1] {
+                if !comment_closing_depths.is_empty() && contains_non_space(&line){
+                    while !comment_closing_depths.is_empty() && current_depth <= comment_closing_depths[comment_closing_depths.len() - 1] {
                         comment_closing_depths.pop();
                         tree.end_child();
-                        end_child_count += 1;
                     }
                 }
             // see if current line creates new comment
                 if line_is_a_comment(&line) {
                     tree.begin_child(line[(count_tabs(&line)*4)+3..].to_string());
-                    begin_child_count += 1;
                     comment_closing_depths.push(current_depth);
                 }
         }
 
-        // println!("begin_child_count: {}", begin_child_count);
-        // println!("end_child_count: {}", end_child_count);
 
-    // turn to StringItem (not sure why)
+    // turn tree to StringItem (not sure why)
     let tree = tree.build();
 
     print_tree(&tree).unwrap();
-
-    // example code
-        // // Build a tree using a TreeBuilder
-        // let tree = TreeBuilder::new("tree".to_string())
-        //     .begin_child("branch".to_string())
-        //         .begin_child("branch2".to_string())
-
-        //             .add_empty_child("empty branch".to_string())
-
-        //             .begin_child("branch2".to_string())
-        //                 .add_empty_child("empty branch".to_string())
-        //             .end_child()
-
-        //             .begin_child("branch2".to_string())
-        //             .end_child()
-
-        //             .begin_child("branch2".to_string())
-        //             .end_child()
-
-        //         .end_child()
-        //     .end_child()
-        //     .add_empty_child("empty branch".to_string())
-        //     .build();
-
-        // // Print out the tree using default formatting
-        // print_tree(&tree).unwrap();
 }
